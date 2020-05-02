a4698d4ac6012b1fff512cee8f33eab334f82942
raeeskkhan <raees20khan@gmail.com>
2020-05-02 20:09:23 +0200

    Initial commit

---
+++ src/khankazanraees/company/Main.java
@@ -0,0 +1,23 @@
+package khankazanraees.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Get n number of terms
+        // n >= 2
+        int n = 10;
+
+        int[] arr = new int[n];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
